c0a457b00d78fb5401c431cf645174f281ae97bd
Ivlev Grigoriy <igs94@mail.ru>
2023-04-24 21:59:06 +0300

    feat: add cards

---
+++ src/components/CardsItem.jsx
@@ -0,0 +1,30 @@
+import IconRemoveCard from '../images/icons/icon-remove-card.svg'
+
+export function CardsItem({ likes, link, name }) {
+  return (
+    <li className="place__item">
+      <button type="button" className="remove-button place__remove-button">
+        <img
+          className="remove-button__img"
+          src={IconRemoveCard}
+          alt="Удалить карточку"
+        />
+      </button>
+
+      <button type="button" className="place__open-popup-button">
+        <img src={link} alt={name} className="place__img" />
+      </button>
+
+      <div className="place__content">
+        <h2 className="place__name">{name}</h2>
+        <div className="">
+          <button
+            type="button"
+            className="like-button place__like-button"
+          ></button>
+          <p className="place__like-amount">{likes.length}</p>
+        </div>
+      </div>
+    </li>
+  )
+}
