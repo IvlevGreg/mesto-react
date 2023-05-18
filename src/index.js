515ddd352ea1b98aa3fd80c22b09d90ec1185e1b
Ivlev Grigoriy <igs94@mail.ru>
2023-05-18 22:07:08 +0300

    feat: add hot update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 
+// eslint-disable-next-line no-undef
+if (module.hot) {
+  // eslint-disable-next-line no-undef
+  module.hot.accept()
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
