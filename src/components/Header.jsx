cff9c25b8daca1474c7253c58e15ebd4d84aa8f0
Ivlev Grigoriy <igs94@mail.ru>
2023-05-29 21:36:43 +0300

    feat: add routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import logo from '../images/logo/logo.svg'
+import {Link, Route, Routes} from "react-router-dom";
+import {ProtectedRouteElement} from "./ProtectedRouteElement";
 
 
-export function Header() {
-return <header className='header'>
-    <a href='#' className='logo'>
-        <img
-            className='logo__img'
-            src={logo}
-            alt='логотип Место'
-        />
-    </a>
-</header>
+export function Header({isLoggedIn, handleLogOut}) {
+    return <header className='header'>
+        <a href='#' className='logo'>
+            <img
+                className='logo__img'
+                src={logo}
+                alt='логотип Место'
+            />
+        </a>
+
+        <Routes>
+            <Route path="/sign-up" element={<Link className='header__link header__text' to='/sign-in'>Войти</Link>}/>
+            <Route path="/sign-in" element={<Link className='header__link header__text' to='/sign-up'>Регистрация</Link>}/>
+            <Route
+                path="/"
+                element={
+                    <ProtectedRouteElement loggedIn={isLoggedIn}>
+                        <div className='header__user'>
+                            <div className='header__text header__email'>email@mail.com</div>
+                            <Link className='header__text header__link header__text_gray' to='/sign-in' onClick={handleLogOut}>Выйти</Link>
+                        < /div>
+                    </ProtectedRouteElement>
+                }
+            />
+        </Routes>
+    </header>
 }
